8affc34c7830fbdce87f3d521af6f973f14789ec
d.dinatale <d.dinatale@tailornet.it>
2022-10-03 15:45:19 +0200

    example single configured

--- example-single-page.sh
+++ example-single-page.sh
@@ -1,7 +1,9 @@
+page=$1
+if [ -z "$1" ]; then echo "insert page!"; exit 1; fi
 ./injector \
     --page-items 5 \
-    --page 1 \
-    --ip 192.168.1.2 \
-    --user user \
-    --pass password \
+    --page $1\
+    --ip 192.168.1.101 \
+    --user kerr \
+    --pass 87GNaswQ \
     --zips-dir certificates
