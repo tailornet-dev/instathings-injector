df4c0f479754888f53ceecfdd99e7ba9aec01108
d.dinatale <d.dinatale@tailornet.it>
2022-10-03 15:50:35 +0200

    typo

--- example-single-page.sh
+++ example-single-page.sh
@@ -2,7 +2,7 @@ page=$1
 if [ -z "$1" ]; then echo "insert page!"; exit 1; fi
 ./injector \
     --page-items 5 \
-    --page $1\
+    --page $1 \
     --ip 192.168.1.101 \
     --user kerr \
     --pass 87GNaswQ \
